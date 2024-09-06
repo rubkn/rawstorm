daa35f3af5aff6b6a5cb46a448bc7ca448f667c4
rubkn <rubenavbrandao@gmail.com>
2024-09-06 18:10:28 +0100

    feat/auth: add dynamodb adapter

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,7 +1,34 @@
 import NextAuth from "next-auth";
-
 import Google from "next-auth/providers/google";
+
+import { DynamoDB, DynamoDBClientConfig } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb";
+import { DynamoDBAdapter } from "@auth/dynamodb-adapter";
+
+const config: DynamoDBClientConfig = {
+  credentials: {
+    accessKeyId: process.env.AUTH_DYNAMODB_ID as string,
+    secretAccessKey: process.env.AUTH_DYNAMODB_SECRET as string,
+  },
+  region: process.env.AUTH_DYNAMODB_REGION,
+};
+
+const client = DynamoDBDocument.from(new DynamoDB(config), {
+  marshallOptions: {
+    convertEmptyValues: true,
+    removeUndefinedValues: true,
+    convertClassInstanceToMap: true,
+  },
+});
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Google],
+  adapter: DynamoDBAdapter(client, {
+    tableName: process.env.DYNAMODB_TABLE_NAME,
+    partitionKey: "pk",
+    sortKey: "sk",
+    indexName: "GSI1",
+    indexPartitionKey: "GSI1PK",
+    indexSortKey: "GSI1SK",
+  }),
 });
