082a65e49f0dc5fa0e133452cf5aa30cbcff810e
rubkn <rubenavbrandao@gmail.com>
2024-09-10 18:16:59 +0100

    feat/s3: upload to S3

---
+++ lib/s3.ts
@@ -0,0 +1,35 @@
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import { v4 as uuidv4 } from "uuid";
+
+type File = {
+  buffer: Buffer;
+  originalFilename: string;
+  mimetype: string;
+};
+
+const s3 = new S3Client({
+  region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID as string,
+    secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY as string,
+  },
+});
+
+export async function uploadToS3(file: File, userId: string) {
+  const photoId = uuidv4();
+  const uniqueFileName = `${userId}/${photoId}-${file.originalFilename}`;
+
+  const uploadParams = {
+    Bucket: process.env.S3_BUCKET_NAME!,
+    Key: uniqueFileName,
+    Body: file.buffer,
+    ContentType: file.mimetype,
+  };
+
+  await s3.send(new PutObjectCommand(uploadParams));
+
+  return {
+    s3Url: `https://${process.env.S3_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/${uniqueFileName}`,
+    photoId,
+  };
+}
