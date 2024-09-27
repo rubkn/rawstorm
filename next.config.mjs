e0ec1d0dc09036ae76800107fd1820227e08d529
rubkn <rubenavbrandao@gmail.com>
2024-09-28 00:46:52 +0100

    chore(theme): update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["rawstorm-stg.s3.eu-west-1.amazonaws.com"],
+  },
+};
 
 export default nextConfig;
