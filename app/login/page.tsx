04be62db35caeb4f388b1677b0d7b891dab3184e
rubkn <rubenavbrandao@gmail.com>
2025-01-02 17:57:43 +0000

    fix: null spotlight photos

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -13,7 +13,7 @@ export default async function LoginPage() {
 
   return (
     <div className="grid min-h-screen w-full grid-cols-1 lg:grid-cols-2">
-      <Spotlight photoUrl={spotlight.photoUrl} userId={spotlight.userId!} />
+      <Spotlight photoUrl={spotlight?.photoUrl} userId={spotlight?.userId} />
 
       <section className="flex flex-col">
         <header className="flex items-center justify-between lg:justify-end p-6">
--- components/spotlight-photo.tsx
+++ components/spotlight-photo.tsx
@@ -6,8 +6,8 @@ export default function SpotlightPhoto({
   photoUrl,
   userId,
 }: {
-  photoUrl: string;
-  userId: string;
+  photoUrl?: string;
+  userId?: string;
 }) {
   return (
     <section className="relative hidden lg:block h-full">
@@ -15,7 +15,7 @@ export default function SpotlightPhoto({
         alt="test"
         width={2000}
         height={2000}
-        src={photoUrl}
+        src={photoUrl!}
         className="w-full h-full object-cover object-center"
       />
       <div className="absolute inset-0 bg-opacity-50 flex flex-col justify-end p-6">
